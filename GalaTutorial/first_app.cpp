e095195331472cb59d144e8bf139451fb2b9e3e7
Flo Maldonado <flo@Aureas-MacBook-Pro.local>
2024-08-08 10:00:17 -0700

    Finished Galea's youtube video: Diffuse Shading - Vulkan Game Engine Tutorial 18

--- GalaTutorial/first_app.cpp
+++ GalaTutorial/first_app.cpp
@@ -69,13 +69,20 @@ namespace lve
 
     void FirstApp::loadGameObjects()
     {
-        std::shared_ptr<LveModel> lveModel = LveModel::createModelFromFile(lveDevice, "/Users/flo/LocalDocuments/Projects/VulkanLearning/GalaTutorial/GalaTutorial/models/smooth_vase.obj");
+        std::shared_ptr<LveModel> lveModel = LveModel::createModelFromFile(lveDevice, "/Users/flo/LocalDocuments/Projects/VulkanLearning/GalaTutorial/GalaTutorial/models/flat_vase.obj");
         
-        auto gameObject = LveGameObject::createGameObject();
-        gameObject.model = lveModel;
-        gameObject.transform.translation = {.0f, .0f, 2.5f};
-        gameObject.transform.scale = glm::vec3(3.f);
-        gameObjects.push_back(std::move(gameObject));
+        auto flatVase = LveGameObject::createGameObject();
+        flatVase.model = lveModel;
+        flatVase.transform.translation = {-.5f, .5f, 2.5f};
+        flatVase.transform.scale = glm::vec3(3.f, 1.5f, 3.f);
+        gameObjects.push_back(std::move(flatVase));
+        
+        lveModel = LveModel::createModelFromFile(lveDevice, "/Users/flo/LocalDocuments/Projects/VulkanLearning/GalaTutorial/GalaTutorial/models/smooth_vase.obj");
+        auto smoothVase = LveGameObject::createGameObject();
+        smoothVase.model = lveModel;
+        smoothVase.transform.translation = {.5f, .5f, 2.5f};
+        smoothVase.transform.scale = glm::vec3(3.f, 1.5f, 3.f);
+        gameObjects.push_back(std::move(smoothVase));
     }
 
 }// namespace lve
