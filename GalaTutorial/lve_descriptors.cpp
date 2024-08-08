f988811b84ea7176e3b574a93c3c0316420efe7b
Flo Maldonado <flo@Aureas-MacBook-Pro.local>
2024-08-08 12:55:02 -0700

    Finished Galea's youtube video: NonCoherentAtomSize Bug Fix (Tutorial 19)

---
+++ GalaTutorial/lve_descriptors.cpp
@@ -0,0 +1,8 @@
+//
+//  lve_descriptors.cpp
+//  GalaTutorial
+//
+//  Created by Flo Maldonado on 8/8/24.
+//
+
+#include "lve_descriptors.hpp"
---
+++ GalaTutorial/lve_descriptors.hpp
@@ -0,0 +1,13 @@
+//
+//  lve_descriptors.hpp
+//  GalaTutorial
+//
+//  Created by Flo Maldonado on 8/8/24.
+//
+
+#ifndef lve_descriptors_hpp
+#define lve_descriptors_hpp
+
+#include <stdio.h>
+
+#endif /* lve_descriptors_hpp */
